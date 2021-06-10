3761c38857fa4e4169feea27df7f316102a0752b
sefdf01 <34137064+sefdf01@users.noreply.github.com>
2021-06-10 10:08:59 +0200

    new tweet

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react'
 import {connect} from 'react-redux'
 import {handleInitialData} from '../actions/shared'
+import Dashboard from '../components/Dashboard'
+import NewTweet from '../components/NewTweet'
+import LoadingBar from 'react-redux-loading'
 
 class App extends Component {
   componentDidMount () {
@@ -9,9 +12,16 @@ class App extends Component {
   render() {
     return (
       <div>
-        Starter Code
+        <LoadingBar/>
+        {this.props.loading === true? null : <NewTweet/>}
       </div>
     )
+  }
+}
+
+function mapStateToProps({ authedUser }){
+  return{
+    loading: authedUser === null
   }
 }
 
